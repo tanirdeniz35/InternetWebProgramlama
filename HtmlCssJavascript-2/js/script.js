a375056f76a47214c2ea1469c62edc490991d030
Deniz TANIR <tanirdeniz35@gmail.com>
2025-03-10 18:56:59 +0300

    css javascript html giriş

---
+++ HtmlCssJavascript-2/js/script.js
@@ -0,0 +1,8 @@
+const jsDiv = document.getElementById("jsBox");
+jsDiv.style.border = "5px solid green";
+jsDiv.innerHTML = "deneme yazı";
+jsDiv.style.color = "white";
+
+const renkDegistir = () => {
+  jsDiv.style.color = "black";
+};
